87e2d480fe3cbebbe2064f2a6f8e6ab39b8e1a8b
kerhbal <kerbaleuclidean@gmail.com>
2020-12-15 11:19:12 -0500

    Update olclean.sh

--- olclean.sh
+++ olclean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-rm -r $OLDATA/nodedata/*
+rm -r $OLDATA/nodedata/chainstate.db
+rm -r $OLDATA/nodedata/lockScriptStore.db
+rm -r $OLDATA/nodedata/validatorJobs.db
 rm -r $OLDATA/consensus/data/*
 rm $OLDATA/*.log
 if [ -e $OLDATA/genesis.json ]
