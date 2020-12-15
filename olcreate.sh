8548ac523ec6d32350d6cc99753a9d49aa55cc3e
kerhbal <kerbaleuclidean@gmail.com>
2020-12-15 09:53:35 -0500

    Update olcreate.sh

--- olcreate.sh
+++ olcreate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pubkey=$(cat consensus/config/priv_validator_key.json | grep -oP -m 1 '(?<="value": ")(.*?)(?=")')
-privkey=$(cat consensus/config/priv_validator_key.json | grep -oP '(?<="value": ")(.*?)(?=")' | tail -n1)
+pubkey=$(cat $OLDATA/consensus/config/priv_validator_key.json | grep -oP -m 1 '(?<="value": ")(.*?)(?=")')
+privkey=$(cat $OLDATA/consensus/config/priv_validator_key.json | grep -oP '(?<="value": ")(.*?)(?=")' | tail -n1)
 
 olclient account add --pubkey $pubkey --privkey $privkey
