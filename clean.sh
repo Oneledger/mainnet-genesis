7a1519f8d764049420ae5066c95fc4026659a13e
manikanta472 <61201964+manikanta472@users.noreply.github.com>
2021-10-17 21:38:52 -0400

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -r $OLDATA/nodedata/chainstate.db
+rm -r $OLDATA/nodedata/lockScriptStore.db
+rm -r $OLDATA/nodedata/validatorJobs.db
+rm -r $OLDATA/consensus/data/*
+rm $OLDATA/*.log
